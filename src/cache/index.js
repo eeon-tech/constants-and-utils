de2aa3695bc5e6ef0b9338eed173a84e7b755aed
Mitch Masia <mjmasia@gmail.com>
2019-11-18 11:45:28 -0600

    feat(cache): adds more cache keys for expiration

--- src/cache/index.js
+++ src/cache/index.js
@@ -1,8 +1,23 @@
+const _fp = require('lodash/fp')
+
+const convertToMS = _fp.multiply(1000)
+
 const prefixSystemList = (key) => `systemLists:${key}`
+
+exports.cachePeriodsInMS = {
+  '1m': convertToMS(60),
+  '1h': convertToMS(3600),
+  '1D': convertToMS(86400),
+  '5D': convertToMS(432000),
+  '10D': convertToMS(864000),
+  '1M': convertToMS(2592000),
+  '6M': convertToMS(15552000),
+}
 
 exports.notableKeys = {
   COMPANY_NEWS: 'companyNews',
   EARNINGS_TODAY: prefixSystemList('earningsToday'),
+  IPOS_TODAY: prefixSystemList('IPOsToday'),
   LATEST_NEWS: prefixSystemList('latestNews'),
   LOSERS: prefixSystemList('losers'),
   MOST_ACTIVE: prefixSystemList('mostActive'),
--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,22 +1,5 @@
 const _ = require('lodash')
-const _fp = require('lodash/fp')
 const utils = require('../utils')
-
-const convertToMS = _fp.multiply(1000)
-
-exports.cleanPeriods = {
-  '1D': _.multiply(24, 3600),
-}
-
-exports.timePeriodsInMS = {
-  '1m': convertToMS(60),
-  '1h': convertToMS(3600),
-  '1D': convertToMS(86400),
-  '5D': convertToMS(432000),
-  '10D': convertToMS(864000),
-  '1M': convertToMS(2592000),
-  '6M': convertToMS(15552000),
-}
 
 exports.timezone = 'America/New_York'
 
