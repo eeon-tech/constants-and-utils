2cfde4023741d54290aec7345960f54c16711ada
Mitch Masia <mjmasia@gmail.com>
2020-01-06 22:45:26 -0500

    feat(macros): adds type label to the macros

--- src/macros/index.js
+++ src/macros/index.js
@@ -1,42 +1,48 @@
 import _ from 'lodash'
 
+const macroTypes = {
+  FACTOR: 'Factor',
+  INDEX: 'Index',
+  SECTOR: 'Sector',
+}
+
 export const macros = {
   factors: [
-    { symbol: 'IWF', label: 'Growth' },
-    { symbol: 'MTUM', label: 'Momentum' },
-    { symbol: 'USMV', label: 'Low Volatility' },
-    { symbol: 'VB', label: 'Small Cap.' },
-    { symbol: 'VIG', label: 'Dividend Appreciation' },
-    { symbol: 'VTI', label: 'Total Stock Market' },
-    { symbol: 'VTV', label: 'Value' },
-    { symbol: 'VUG', label: 'Large Cap Growth' },
-    { symbol: 'VYM', label: 'High Dividend' },
+    { symbol: 'IWF', label: 'Growth', type: macroTypes.FACTOR },
+    { symbol: 'MTUM', label: 'Momentum', type: macroTypes.FACTOR },
+    { symbol: 'USMV', label: 'Low Volatility', type: macroTypes.FACTOR },
+    { symbol: 'VB', label: 'Small Cap.', type: macroTypes.FACTOR },
+    { symbol: 'VIG', label: 'Dividend Appreciation', type: macroTypes.FACTOR },
+    { symbol: 'VTI', label: 'Total Stock Market', type: macroTypes.FACTOR },
+    { symbol: 'VTV', label: 'Value', type: macroTypes.FACTOR },
+    { symbol: 'VUG', label: 'Large Cap Growth', type: macroTypes.FACTOR },
+    { symbol: 'VYM', label: 'High Dividend', type: macroTypes.FACTOR },
   ],
   indices: [
-    { symbol: 'SPY', label: 'S&P 500' },
-    { symbol: 'DIA', label: 'Dow Jones' },
-    { symbol: 'IWM', label: 'Russell 2K' },
-    { symbol: 'QQQ', label: 'NASDAQ 100 ' },
+    { symbol: 'SPY', label: 'S&P 500', type: macroTypes.INDEX },
+    { symbol: 'DIA', label: 'Dow Jones', type: macroTypes.INDEX },
+    { symbol: 'IWM', label: 'Russell 2K', type: macroTypes.INDEX },
+    { symbol: 'QQQ', label: 'NASDAQ 100 ', type: macroTypes.INDEX },
   ],
   sectors: [
-    { symbol: 'XLB', label: 'Materials' },
-    { symbol: 'XLE', label: 'Energy' },
-    { symbol: 'XLF', label: 'Financials' },
-    { symbol: 'XLI', label: 'Industrials' },
-    { symbol: 'XLV', label: 'Healthcare' },
-    { symbol: 'XLC', label: 'Communications' },
-    { symbol: 'XLY', label: 'Cons. Discretionary' },
-    { symbol: 'XLP', label: 'Cons. Staples' },
-    { symbol: 'XLU', label: 'Utilities' },
-    { symbol: 'XLK', label: 'Technology' },
-    { symbol: 'XLRE', label: 'Real Estate' },
+    { symbol: 'XLB', label: 'Materials', type: macroTypes.SECTOR },
+    { symbol: 'XLE', label: 'Energy', type: macroTypes.SECTOR },
+    { symbol: 'XLF', label: 'Financials', type: macroTypes.SECTOR },
+    { symbol: 'XLI', label: 'Industrials', type: macroTypes.SECTOR },
+    { symbol: 'XLV', label: 'Healthcare', type: macroTypes.SECTOR },
+    { symbol: 'XLC', label: 'Communications', type: macroTypes.SECTOR },
+    { symbol: 'XLY', label: 'Cons. Discretionary', type: macroTypes.SECTOR },
+    { symbol: 'XLP', label: 'Cons. Staples', type: macroTypes.SECTOR },
+    { symbol: 'XLU', label: 'Utilities', type: macroTypes.SECTOR },
+    { symbol: 'XLK', label: 'Technology', type: macroTypes.SECTOR },
+    { symbol: 'XLRE', label: 'Real Estate', type: macroTypes.SECTOR },
   ],
 }
 
 export const macrosList = [
-  ...macros.factors,
   ...macros.indices,
   ...macros.sectors,
+  ...macros.factors,
 ]
 
 export const getMacroBySymbol = (symbol) => _.find(macrosList, { symbol })
